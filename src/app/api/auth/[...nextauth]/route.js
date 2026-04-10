2ffd226f8bce5702e5e473a7cea618f4e3ff7a66
amitvishwa19 <amitvishwa19@gmail.com>
2026-04-10 18:47:11 +0530

    removed crystal aura

--- src/app/api/auth/[...nextauth]/route.js
+++
@@ -1,8 +0,0 @@
-import NextAuthImport from "next-auth"
-import { authOptions } from "./options"
-
-const NextAuth = NextAuthImport.default || NextAuthImport
-
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
--- src/app/workspace/[workspaceId]/system/access/_components/permission/Permissions.jsx
+++
@@ -1,7 +0,0 @@
-import React from'react'
-
-export default function Permissions() {
- return (
- <div>Permissions</div>
- )
-}
