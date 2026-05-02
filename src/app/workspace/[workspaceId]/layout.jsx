3c29d6d177d6a1566481dde8f54385d808775aa5
amitvishwa19 <amitvishwa19@gmail.com>
2026-05-02 16:46:06 +0530

    product

--- src/app/workspace/[workspaceId]/layout.jsx
+++ src/app/workspace/[workspaceId]/layout.jsx
@@ -6,27 +6,33 @@ import { Inter } from "next/font/google";
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import AppTopNav from '../_components/AppTopNav';
 import AppSidebar from '../_components/AppSidebar';
+import { AppProvider } from '@/providers/AppProvider';
+import { ThemeProvider } from '@/providers/ThemeProvider';
 const font = Inter({ subsets: ["latin"] });
 
 export default function WorkspaceLayout({ children }) {
     return (
-        <div className={`flex h-dvh max-w-[100vw] ${font.className} overflow-hidden bg-background`}>
-            <SidebarProvider>
-                <AppSidebar />
-                <SidebarInset className='flex flex-col w-full h-full transition-all p-2'>
-                    <div className="p-2">
-                        <AppTopNav />
-                    </div>
-                    <div className='flex-1 min-h-0 relative pt-0 overflow-hidden'>
-                        <div className='h-full relative border border-border rounded-md bg-card/50 overflow-hidden shadow-soft'>
-                            <ScrollArea className="h-full overflow-hidden">
-                                {children}
-                                <ScrollBar orientation="horizontal" />
-                            </ScrollArea>
-                        </div>
-                    </div>
-                </SidebarInset>
-            </SidebarProvider>
-        </div>
+        <AppProvider>
+            <ThemeProvider>
+                <div className={`flex h-dvh max-w-[100vw] ${font.className} overflow-hidden bg-background`}>
+                    <SidebarProvider>
+                        <AppSidebar />
+                        <SidebarInset className='flex flex-col w-full h-full transition-all p-2'>
+                            <div className="p-2">
+                                <AppTopNav />
+                            </div>
+                            <div className='flex-1 min-h-0 relative pt-0 overflow-hidden'>
+                                <div className='h-full relative border border-border rounded-md bg-card/50 overflow-hidden shadow-soft'>
+                                    <ScrollArea className="h-full overflow-hidden">
+                                        {children}
+                                        <ScrollBar orientation="horizontal" />
+                                    </ScrollArea>
+                                </div>
+                            </div>
+                        </SidebarInset>
+                    </SidebarProvider>
+                </div>
+            </ThemeProvider>
+        </AppProvider>
     )
 }
